56e6cab960139bba88bbf36db641d6a87ae8d67f
Shahryar Valizadeh <sv517@georgetown.edu>
2022-08-03 01:55:50 -0400

    Added Font Awesome to complete implementation of the footer

--- components/footer.js
+++ components/footer.js
@@ -1,12 +1,31 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+
+
 const Footer = () => {
+    // returns copyright and social media links
     return (
         <footer>
-            <h6>Hello from the footer</h6>
-            <br />
-            <div>
-                Copyright &copy; {new Date().getFullYear()} Shahryar Valizadeh
+            <div className="footer-social">
+                <a href="https://www.linkedin.com/in/shahryarvalizadeh/" target="_blank" rel="noopener noreferrer" className='linkedin'>
+                    <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+                <a href="https://github.com/ShahV517" target="_blank" rel="noopener noreferrer" className='github'>
+                    <FontAwesomeIcon icon={faGithub} />
+                </a>
+                <a href="https://www.instagram.com/shahryar_v/" target="_blank" rel="noopener noreferrer" className='instagram'>
+                    <FontAwesomeIcon icon={faInstagram} />
+                </a>
+                <a href="https://twitter.com/realslickshah" target="_blank" rel="noopener noreferrer" className='twitter'>
+                    <FontAwesomeIcon icon={faTwitter} />
+                </a>
+            </div>
+            <div className="footer-copyright">
+                &copy; {new Date().getFullYear()} Copyright Shahryar Valizadeh
             </div>
         </footer>
     );
     }
-    export default Footer;
+    
+export default Footer;
